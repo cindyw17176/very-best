ffdee223ea90372c852ea92f74aaecc9772dff5f
Sean Xu <xxu30@chicagobooth.edu>
2018-12-07 22:26:57 +0000

    75% done

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,7 @@
 class VenuesController < ApplicationController
   def index
     @q = Venue.ransack(params.fetch("q", nil))
+    
     @venues = @q.result(:distinct => true).includes(:bookmarks, :neighborhood, :fans, :specialties).page(params.fetch("page", nil)).per(10)
     @dish = Dish.all
     @cuisine = Cuisine.all
